7959c4f990404c5211ae22ef51f39c05bd578a3d
owaisj <jamil.owais@gmail.com>
2019-04-05 00:17:25 -0500

    AJAX code added with test Pokemon

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,17 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
+    function displayPokemon(pokemon) {
+        
+        var queryURL = "https://pokeapi.co/api/v2/pokemon/" + pokemon;
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
+        })
+
+    }
+
+    displayPokemon("haxorus");
 });
